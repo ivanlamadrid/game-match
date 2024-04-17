ceef5b06fddd7aa4a675d47ea630d7beb918a790
Ivan La Madrid <ivanlamadrid2004@gmail.com>
2024-04-17 16:31:40 -0500

    Se ha configurado la camara

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,11 +6,24 @@ public class Board : MonoBehaviour
 {
     public int width, height;
     public GameObject titleObject;
+
+    public float cameraSizeOffSet;
+    public float cameraVerticalOffSet;
     void Start()
     {
         SetupBoard();
+        CameraPosition();
     }
 
+    private void CameraPosition()
+    {
+        float newX = (float)width / 2f;
+        float newY = (float)height / 2f;
+        if (Camera.main != null) Camera.main.transform.position = new Vector3(newX -0.5f, newY - 0.5f + cameraVerticalOffSet, -10f);
+        float horizontal = width + 1;
+        float vertical = ((float)height/2) + 1;
+        if (Camera.main != null) Camera.main.orthographicSize = horizontal > vertical ? horizontal + cameraSizeOffSet : vertical + cameraSizeOffSet;
+    }
     private void SetupBoard()
     {
         for (int x = 0; x < width; x++)
